7727e3dcc52272c5940930456d5820bd9344bd73
Harry <virtualslide@gmail.com>
2022-10-18 11:01:07 +0100

    added url generator

--- nodeBackEnd/Controllers/exchangeController.js
+++ nodeBackEnd/Controllers/exchangeController.js
@@ -1,13 +1,14 @@
 const DbService = require('../Services/DbService')
 
 const verifyDateIsFuture = (date) => {
-    console.log(date)
-
     const today = new Date()
     const inputDate = new Date(date)
-    console.log(today)
-    console.log(inputDate)
     return inputDate > today
+}
+
+async function generateUrl() {
+    const {randomBytes} = await import('node:crypto')
+    return randomBytes(8).toString('hex')
 }
 
 async function createExchange(req, res) {
@@ -15,11 +16,15 @@ async function createExchange(req, res) {
         message: "Failed to create reminder due to incorrect input",
         data: [{}]
     }
+    const adminUrl = await generateUrl()
+    const participantUrl = await generateUrl()
     const collection = await DbService.connectToDb()
     const newExchangeData = {
         exchangeName: req.body.data.exchangeName,
         exchangeDate: req.body.data.exchangeDate,
         isPostal: req.body.data.isPostal,
+        adminUrl: adminUrl,
+        participantUrl: participantUrl,
         participants: {}
     }
     const isFuture = verifyDateIsFuture(newExchangeData.exchangeDate)
@@ -29,7 +34,10 @@ async function createExchange(req, res) {
         if (result.acknowledged) {
             const responseData = {
                 message: "Successfully created exchange",
-                data: [{}]
+                data: {
+                    participantUrl: participantUrl,
+                    adminUrl: adminUrl
+                }
             }
             res.status(200).json(responseData)
         } else {
