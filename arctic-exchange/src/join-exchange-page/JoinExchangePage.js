2bbb57ed04f707829b8f7a3f478a0193e88a099b
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-10-18 16:55:31 +0100

    some lovely stuff

--- arctic-exchange/src/join-exchange-page/JoinExchangePage.js
+++ arctic-exchange/src/join-exchange-page/JoinExchangePage.js
@@ -1,7 +1,47 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
+import "./JoinExchangePage.css"
 
 const JoinExchangePage = () => {
+    const [exchangeName, setExchangeName] = useState('')
+    const [date, setDate] = useState('')
+    const [url, setUrl] = useState('')
 
+    useEffect(() => {
+        fetch('http://localhost:3002' + 'join/:participantUrl' , )
+            .then((response) => {
+            return response.json()
+        }).then((data) => {
+            setExchangeName(data.exchangeName)
+            setDate(data.exchangeDate)
+            setUrl(data.participantUrl)
+            })
+    }, [])
+
+    return (
+        <div className={"joinPageComponents"}>
+            <div className={"exchangeDetails"}>
+                <h2>{exchangeName}</h2>
+                <h3>{date}</h3>
+                <p>Your private URL is</p>
+                <p>{url}</p>
+            </div>
+            <div className={"addParticipant"}>
+                <h2>Add New Participant</h2>
+                <form className={"exchangeForm"}>
+                    {/*<input type={"name"} value={name} onChange={handleChangeName} required/>*/}
+                    <input type={"type"} />
+                    <input type={"type"} />
+                    <input type={"type"} />
+                    <p></p>
+                    <input type={"submit"} name={"submit"} className={"submit"} />
+                </form>
+            </div>
+            <div className={"currentlyRegistered"}>
+                <h2>Currently registered</h2>
+            </div>
+        </div>
+
+    )
 }
 
 export default JoinExchangePage
