c7d660fb48931726c6d9bf715d817c1f75d8d47c
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-10-19 16:10:22 +0100

    this is the start of the users page

--- arctic-exchange/src/current-participant-page/CurrentParticipantPage.js
+++ arctic-exchange/src/current-participant-page/CurrentParticipantPage.js
@@ -1,6 +1,18 @@
+import {useParams} from "react-router-dom"
+import {useEffect, useState} from "react"
 
 const CurrentParticipantPage = () => {
+    const {participantUrl} = useParams()
+    const [usersArray, setUsersArray] = useState('')
 
+    useEffect(() => {
+        fetch('https://localhost:3002/join' + participantUrl)
+            .then((response ) => {
+                return response.json()
+            }).then((data) => {
+                setUsersArray(data.data.participants)
+            })
+    }, [])
 }
 
 export default CurrentParticipantPage
