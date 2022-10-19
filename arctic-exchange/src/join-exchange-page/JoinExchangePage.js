5edaee348ff77de46b232cfcc3430be8629eb7a7
tom <thomarhughes98@gmail.com>
2022-10-19 14:22:51 +0100

    second form and page done

--- arctic-exchange/src/join-exchange-page/JoinExchangePage.js
+++ arctic-exchange/src/join-exchange-page/JoinExchangePage.js
@@ -6,7 +6,23 @@ const JoinExchangePage = () => {
     const [exchangeName, setExchangeName] = useState('')
     const [date, setDate] = useState('')
     const [url, setUrl] = useState('')
+    const [exchangeId, setExchangeId] = useState('')
     const {participantUrl} = useParams()
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [address, setAddress] = useState('')
+
+    const handleChangeName = (event) => {
+        setName(event.target.value)
+    }
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const handleChangeAddress = (event) => {
+        setAddress(event.target.value)
+    }
 
     useEffect(() => {
         fetch('http://localhost:3002/join/' + participantUrl)
@@ -16,36 +32,63 @@ const JoinExchangePage = () => {
             setExchangeName(data.data.exchangeName)
             setDate(data.data.exchangeDate)
             setUrl(data.data.participantUrl)
+            setExchangeId(data.data._id)
             })
     }, [])
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const requestBody = JSON.stringify({
+            message: "",
+            data: {
+                name: name,
+                email: email,
+                address: address,
+                _id: exchangeId
+            }
+        })
+        fetch('http://localhost:3002' + '/join', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: requestBody
+        }).then(response => {
+            const statusCode = response.status,
+                success = response.ok
+
+            response.json().then(response => {
+                if(statusCode === 400) {
+                    return alert('FAIL!')
+                } else {
+                    return alert('NO FAIL!')
+                }
+            })
+        })
+    }
 
     return (
         <div className={"joinPageComponents"}>
             <div className={"exchangeDetails"}>
                 <h2>{exchangeName}</h2>
                 <h3>{date}</h3>
-                <p>Your URL is</p>
-                <p>http://arcticexchange.com/{url}</p>
             </div>
             <div className={"addParticipant"}>
                 <h2>Add New Participant</h2>
-                <form className={"addParticipantForm"}>
-                    {/*<input type={"name"} value={name} onChange={handleChangeName} required/>*/}
-                    <input type={"type"} className={"input"}/>
-                    <input type={"type"} className={"input"}/>
-                    <input type={"type"} className={"input"}/>
+                <form className={"addParticipantForm"} onSubmit={handleSubmit}>
+                    <input type={"name"} className={"name"} value={name} onChange={handleChangeName} required />
+                    <input type={"email"} className={"email"} value={email} onChange={handleChangeEmail} required />
+                    <input type={"address"} className={"address"} value={address} onChange={handleChangeAddress} required />
                     <p></p>
                     <input type={"submit"} name={"submit"} className={"submit"} />
                 </form>
             </div>
-            <div className={"currentlyRegistered"}>
-                <h2>Currently registered</h2>
-                {/*<p>blah</p>*/}
-                {/*<p>blah</p>*/}
-                {/*<p>blah</p>*/}
-                {/*<p>blah</p>*/}
-                {/*<p>blah</p>*/}
-            </div>
+            {/*<div className={"currentlyRegistered"}>*/}
+            {/*    <h2>Currently registered</h2>*/}
+            {/*    /!*<p>blah</p>*!/*/}
+            {/*    /!*<p>blah</p>*!/*/}
+            {/*    /!*<p>blah</p>*!/*/}
+            {/*    /!*<p>blah</p>*!/*/}
+            {/*    /!*<p>blah</p>*!/*/}
+            {/*</div>*/}
         </div>
 
     )
