25d1de704212437215d6716298216c0b1de0eb49
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2022-10-19 16:58:31 +0100

    created currentparticipantpage, not showing on page

--- arctic-exchange/src/join-exchange-page/JoinExchangePage.js
+++ arctic-exchange/src/join-exchange-page/JoinExchangePage.js
@@ -77,8 +77,8 @@ const JoinExchangePage = () => {
                 <h2>Add New Participant</h2>
                 <form className={"addParticipantForm"} onSubmit={handleSubmit}>
                     <input type={"name"} className={"name"} value={name} onChange={handleChangeName} placeholder={"name"} required />
-                    <input type={"email"} className={"email"} value={email} onChange={handleChangeEmail} placeholder={"email"}required />
-                    <input type={"address"} className={"address"} value={address} onChange={handleChangeAddress} placeholder={"address"}required />
+                    <input type={"email"} className={"email"} value={email} onChange={handleChangeEmail} placeholder={"email"} required />
+                    <input type={"address"} className={"address"} value={address} onChange={handleChangeAddress} placeholder={"address"} required />
                     <p></p>
                     <input type={"submit"} name={"submit"} className={"submit"} />
                 </form>
